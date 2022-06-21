1962fe7bd0ab746e871ad683f46a87e60947c09a
danilsyah <danilsyaharihardjo@gmail.com>
2022-06-21 22:34:15 +0700

    create .env for secret key django

--- todoapp/backend/core/settings.py
+++ todoapp/backend/core/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
+from datetime import timedelta
 from pathlib import Path
+from .env import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(jxm7nr#@p&6!=i=+8ux7=dkx-46t@wqw-2@t4#v-=s8%$wkf_'
+SECRET_KEY = get_env_value('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
