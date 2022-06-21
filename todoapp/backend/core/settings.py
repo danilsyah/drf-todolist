88c97c39ccb2b683d5c4fe7b92eee7c41e1bd5de
danilsyah <danilsyaharihardjo@gmail.com>
2022-06-21 23:59:05 +0700

    add endpoint api,create crud todo, update complete todo

--- todoapp/backend/core/settings.py
+++ todoapp/backend/core/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.todo'
+    'rest_framework',
+    'apps.todo',
+    'apps.api',
 ]
 
 MIDDLEWARE = [
@@ -107,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'id-ID'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Jakarta'
 
 USE_I18N = True
 
